692add48da712368b5acd0c20132c46a8b00cf3d
stellarator-x <admathur@iitk.ac.in>
2020-04-19 05:11:27 +0530

    fix errs

--- obs-avd/GaussianPF_PathPlanning.cpp
+++ obs-avd/GaussianPF_PathPlanning.cpp
@@ -11,43 +11,55 @@ namespace plt = matplotlibcpp;
 using namespace std;
 
 
+void draw_circle(float cx, float cy, float rad){
+    vector<float> x;
+    vector<float> y;
+    for(int i =0;i<360;i++){
+        float a = index_to_angle(i);
+        x.push_back(cx+rad*cos(a));
+        y.push_back(cy+rad*sin(a));
+    }
+    plt::plot(x, y);
+}
+
 vector<obstacle> get_obstacles(vector<float> polar_dat){
     vector<obstacle> obs;
     int flag = 0; // represents whether an obs is being read
-    float theta1;
+    float theta1, theta2, angular_width, mean_angle, dist, d;
+    polar_dat[polar_dat.size()-1] = MAX_DIST;
     for(int i=0;i<polar_dat.size();i++){
-        auto dist = polar_dat[i];
-        switch(flag - (int)(dist<MAX_DIST)){
-            case -1 : // Start a new  obstacle
-                flag = 1;
-                theta1 = i;
-                break;
-            case 1: // End of current obstacle
-              {  flag = 0;
-                float theta2 = i-1;
-                float anglular_width = theta2-theta1;
-                float mean_angle = (theta2+theta1)/2;
-                float dist = accumulate(&polar_dat[theta1], &polar_dat[theta2], 0.0f) / (theta2-theta1);
-                assert(anglular_width >= 0);
-                // if(temp.anglular_width > 0) // Ignoring noise?
-                    obs.push_back(obstacle(dist, anglular_width, mean_angle));
-                break;}
-            default: // Continue as is for 0, 2
-                break;
+        dist = polar_dat[i];
+
+        if((flag == 0) and (dist<MAX_DIST)){// Start a new  obstacle
+            flag = 1;
+            theta1 = i;
         }
+
+        else if((flag == 1) and (dist>=MAX_DIST)){
+            flag = 0;
+            theta2 = i-1;
+            angular_width = theta2-theta1;
+            mean_angle = (theta2+theta1)/2;
+            d = accumulate(&polar_dat[theta1], &polar_dat[theta2], 0.0f) / angular_width;
+            assert(angular_width >= 0);
+            obs.push_back(obstacle(d, angular_width, mean_angle));
+            theta1 = theta2;
+        }         
     }
+    
     return obs;
 }
 
-vector<obstacle> process_obs(vector<obstacle> obs){
-    for (obstacle o : obs){
-        o.increase_width(WIDTH);
+vector<obstacle> process_obs(vector<obstacle> &obs){
+    vector<obstacle> obs_  = obs;
+    for (int i = 0; i<obs_.size();i++){
+        obs_[i].increase_width(WIDTH);
     }
-    return obs;
+    return obs_;
 }
 
 int get_best_header(vector<float> potential){
-    int header = std::distance(potential.begin()+1, std::min_element(potential.begin(), potential.end()));
+    int header = std::distance(potential.begin(), std::min_element(potential.begin(), potential.end()));
     return header;
 }
 
@@ -58,17 +70,19 @@ float get_header_rad(vector<float> polardat, float goal_angle){
     float header;
     
     auto obstacles = get_obstacles(polardat);
-    obstacles = process_obs(obstacles);
+    // obstacles = process_obs(obstacles);
     // For debugging
     for(auto a:obstacles){
         cout << "Obstacle is " << a.get_dist()<< "m away at angle "<<a.get_theta()<<" with width "<<a.get_phi()<<" \n";
     }
     cout << "Num obstacles = " << obstacles.size() << endl;
     vector<float> potfield(360, 0);
-    for(auto ob: obstacles){
-        ob.compute_field(potfield);
+    for(int i=0;i<obstacles.size();i++){
+        obstacles[i].compute_field(potfield);
     }
     goal_field(potfield, goal_angle);
+    // plt::plot(potfield);
+    // plt::show();
     header = get_best_header(potfield);
 
     return index_to_angle(header);
