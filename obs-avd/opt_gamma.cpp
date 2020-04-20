cb535aa112dc5238a8a23d31671e411a10eb4e9f
stellarator-x <admathur@iitk.ac.in>
2020-04-20 19:11:23 +0530

    determining gamma

--- obs-avd/opt_gamma.cpp
+++ obs-avd/opt_gamma.cpp
@@ -10,27 +10,27 @@ int main(){
     vector<float> gammas;
     vector<float> rewards;
 
-    // for(float i=0;i<=1.5;i+=0.01){
-    //     cout << "Gamma = " << i ;
-    //     gammas.push_back(i);
-    //     float reward = find_path(i, false);
-    //     cout << ", Reward = " << reward << endl;
-    //     rewards.push_back(reward);
+    for(float i=0;i<=1.5;i+=0.01){
+        cout << "Gamma = " << i ;
+        gammas.push_back(i);
+        float reward = find_path(i, false);
+        cout << ", Reward = " << reward << endl;
+        rewards.push_back(reward);
 
-    // }
-    // plt::plot(gammas, rewards);
-    // plt::xlabel("Gamma");
-    // plt::ylabel("Rewards");
-    // plt::title("Rewards vs Gamma");
-    // int i_max = std::distance(rewards.end(), std::max_element(rewards.begin(), rewards.end()));
-    // i_max = rewards.size() + i_max;
-    // vector<float> g_max = {gammas[i_max]};
-    // vector<float> r_max = {rewards[i_max]};
-    // plt::scatter(g_max, r_max);
-    // plt::show();
+    }
+    plt::plot(gammas, rewards);
+    plt::xlabel("Gamma");
+    plt::ylabel("Rewards");
+    plt::title("Rewards vs Gamma");
+    int i_max = std::distance(rewards.end(), std::max_element(rewards.begin(), rewards.end()));
+    i_max = rewards.size() + i_max;
+    vector<float> g_max = {gammas[i_max]};
+    vector<float> r_max = {rewards[i_max]};
+    plt::scatter(g_max, r_max);
+    plt::show();
 
     // Showing the path with max reward
-    find_path(1, true);
+    find_path(g_max[0], true);
     // cout << "Best gamma = " << g_max[0]<< endl;
 
     return 0;
